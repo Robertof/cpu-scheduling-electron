289a9c298f268798d0f1aa048dd325e113b76a4e
Roberto Frenna <roberto.frenna@gmail.com>
2018-11-20 10:50:40 +0100

    first commit using electron-vue

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: 'eslint:recommended',
+  globals: {
+    __static: true
+  },
+  plugins: [
+    'html'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'no-console': [
+      0
+    ],
+    
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
+  /*'rules': {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }*/
+}
